d657750eaee3a5c331e1fb5203bf1f5826cf2124
ByronWLitle <bwlitle@amarillocollege.com>
2024-09-09 13:24:06 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            //Prompts user to give 3 numbers
+            Console.WriteLine("Enter 3 numbers: ");
+            //Converts user input to three variables for comparison
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            num3 = Convert.ToInt32(Console.ReadLine());
+            //Stores numbers in array to allow for comparison
+            int[] myNumbers = { num1, num2, num3 };
+            //Divides space between prompts
+            Console.WriteLine("");
+            //Outputs largest value with myNumbers array
+            Console.WriteLine(myNumbers.Max());
+            //Pauses program to view
+            Console.ReadLine();
+        }
+    }
+}
